f22eccefe50673488192c24bbd7598f388a0b67c
dcschiba <dcs.chiba@gmail.com>
2017-12-06 14:13:21 +0900

    add client app

---
+++ client/app.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+    var url = 'https://catalyst-push.glitch.me/notification/devices';
+
+    $.ajax({
+        url: url,
+        dataType: "json",
+        success: function (data) {
+            for (var i in data.devices) {
+                $('#device').append($('<option></option>')
+                    .val(data.devices[i].uuid)
+                    .text(data.devices[i].uuid));
+            }
+        },
+        error: function (err) {
+            console.error(err);
+            alert('connection error');
+        },
+    });
+})
+
+$('#post').click(function () {
+    var data = {
+        uuid: $('#topForm [name=uuid]').val(),
+        title: $('#topForm [name=text-title]').val(),
+        message: $('#topForm [name=text-body]').val(),
+        path: $('#topForm [name=radio-path]:checked').val(),
+    };
+    var url = 'https://catalyst-push.glitch.me/notification/send';
+    $.ajax({
+        url: url,
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(data),
+        success: function () {
+            alert('success');
+        },
+        error: function (err) {
+            console.error(err);
+            alert('connection error');
+        },
+    })
+});
--- index.js
+++ index.js
@@ -2,7 +2,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const admin = require('firebase-admin');
 const Datastore = require('nedb');
+const log4js = require('log4js');
 const serviceAccount = require('./serviceAccountKey.json');
+
+// Logger
+const logger = log4js.getLogger();
+logger.level = 'info';
+
+// Error Catch
+process.on('uncaughtException', (err) => {
+  logger.error('unhandled exception has occured');
+  logger.error(err);
+});
 
 // initialize FCM
 admin.initializeApp({
@@ -19,29 +30,39 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
+  res.header('Content-Type', 'application/json; charset=utf-8');
   next();
 });
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(express.static('client'));
+
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Listening on port ${port} ...`);
+  logger.info(`listening on port ${port} ...`);
 });
 
+// get device list
 app.get('/notification/devices', (req, res) => {
   db.devices.find({}, (err, devices) => {
     if (err) {
-      res.header('Content-Type', 'application/json; charset=utf-8');
       res.status(500);
-      return res.json({ result: 'error', message: err });
+      const response = {
+        error: {
+          message: err,
+        },
+      };
+      logger.error(err);
+      return res.json(response);
     }
-    res.header('Content-Type', 'application/json; charset=utf-8');
     res.status(200);
+    logger.info('getting device list succeeded', devices);
     return res.json({ devices });
   });
 });
 
+// regist device
 app.post('/notification/regist', (req, res) => {
   const { token, uuid, platform } = req.body;
   const device = {
@@ -49,39 +70,92 @@ app.post('/notification/regist', (req, res) => {
     uuid,
     platform,
   };
-  db.devices.insert(device, (err) => {
+
+  if (!token || !uuid || !platform) {
+    res.status(500);
+    const response = {
+      error: {
+        message: 'registration data is missing',
+      },
+    };
+    logger.error(response.error.message, device);
+    return res.json(response);
+  }
+
+  const options = {
+    upsert: true,
+  };
+
+  db.devices.update({ uuid }, device, options, (err) => {
     if (err) {
-      res.header('Content-Type', 'application/json; charset=utf-8');
       res.status(500);
-      return res.json({ result: 'error', message: err });
+      const response = {
+        error: {
+          message: err,
+        },
+      };
+      logger.error(response.error.message);
+      return res.json(response);
     }
-    res.header('Content-Type', 'application/json; charset=utf-8');
     res.status(200);
+    logger.info('registration succeeded', device);
     return res.json({ result: 'success' });
   });
 });
 
+// send push
 app.post('/notification/send', (req, res) => {
   const { uuid } = req.body;
+  if (!uuid) {
+    res.status(500);
+    const response = {
+      error: {
+        message: 'uuid is missing',
+      },
+    };
+    logger.error(response.error.message, uuid);
+    return res.json(response);
+  }
+
+
   db.devices.findOne({ uuid }, (err, device) => {
     if (err) {
-      res.header('Content-Type', 'application/json; charset=utf-8');
       res.status(500);
-      return res.json({ result: 'error', message: err });
+      const response = {
+        error: {
+          message: err,
+        },
+      };
+      logger.error(response.error.message);
+      return res.json(response);
     } else if (device === null) {
-      res.header('Content-Type', 'application/json; charset=utf-8');
       res.status(500);
-      return res.json({ result: 'error', message: 'not found target device' });
+      const response = {
+        error: {
+          message: 'target device was not found',
+        },
+      };
+      logger.error(response.error.message, device);
+      return res.json(response);
     }
 
-    const { token, platform } = device;
-    const { title, message } = req.body;
+    const {
+      token,
+      platform,
+    } = device;
+    const {
+      title,
+      message,
+      path = '',
+    } = req.body;
+
     let payload;
     if (platform === 'Android') {
       payload = {
         data: {
           title,
           message,
+          path,
         },
       };
     } else if (platform === 'iOS') {
@@ -90,19 +164,34 @@ app.post('/notification/send', (req, res) => {
           title,
           body: message,
         },
+        data: {
+          path,
+        },
       };
+    } else {
+      res.status(500);
+      const response = {
+        error: {
+          message: `${platform} is not supported`,
+        },
+      };
+      logger.error(response.error.message, device);
+      return res.json(response);
     }
 
-    admin.messaging().sendToDevice(token, payload)
-      .then(() => {
-        res.header('Content-Type', 'application/json; charset=utf-8');
-        res.status(200);
-        return res.json({ result: 'success' });
-      })
-      .catch((error) => {
-        res.header('Content-Type', 'application/json; charset=utf-8');
-        res.status(500);
-        return res.json({ result: 'error', message: error });
-      });
+    admin.messaging().sendToDevice(token, payload).then(() => {
+      res.status(200);
+      logger.info('notification succeeded', payload);
+      return res.json({ result: 'success' });
+    }).catch((e) => {
+      res.status(500);
+      const response = {
+        error: {
+          message: e,
+        },
+      };
+      logger.error(response.error.message);
+      return res.json(response);
+    });
   });
 });
